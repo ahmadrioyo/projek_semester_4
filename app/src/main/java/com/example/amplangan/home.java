b17c8fd2c80597048061226faaf6e27db0212c2f
ndyysagiii <118824217+ndyysagiii@users.noreply.github.com>
2023-05-20 13:34:42 +0700

    intent profil

--- app/src/main/java/com/example/amplangan/home.java
+++ app/src/main/java/com/example/amplangan/home.java
@@ -2,13 +2,27 @@ package com.example.amplangan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
+    ImageView profil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        profil = (Button) findViewById(R.id.button_masuk);
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resgisterIntent = new Intent(home.this, profil.class);
+                startActivity(resgisterIntent);
+                overridePendingTransition(0,0);
+            }
+        });
     }
 }
