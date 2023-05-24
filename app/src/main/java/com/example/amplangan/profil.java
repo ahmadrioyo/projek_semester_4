dceaa17c24523da78fc354a4570f585c1ef78f30
ndyysagiii <118824217+ndyysagiii@users.noreply.github.com>
2023-05-24 07:20:29 +0700

    Intent profil

--- app/src/main/java/com/example/amplangan/profil.java
+++ app/src/main/java/com/example/amplangan/profil.java
@@ -2,13 +2,51 @@ package com.example.amplangan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class profil extends AppCompatActivity {
+    ImageView cross;
+    ImageView cek;
+    Button simpan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        cross = (ImageView) findViewById(R.id.crossx);
+        cross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resgisterIntent = new Intent(profil.this, home.class);
+                startActivity(resgisterIntent);
+                overridePendingTransition(0,0);
+            }
+        });
+
+        cek = (ImageView) findViewById(R.id.cekl);
+        cek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resgisterIntent = new Intent(profil.this, home.class);
+                startActivity(resgisterIntent);
+                overridePendingTransition(0,0);
+            }
+        });
+
+        simpan = (Button) findViewById(R.id.simpan1);
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resgisterIntent = new Intent(profil.this, home.class);
+                startActivity(resgisterIntent);
+                overridePendingTransition(0,0);
+            }
+        });
+
     }
 }
